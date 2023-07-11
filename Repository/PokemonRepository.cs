ccbd861b3abd3c8384935e6ead1a8a6fbefc3223
trieu369 <trieu@saigon.digital>
2023-07-11 08:29:46 +0700

    finished-controllers

---
+++ Repository/PokemonRepository.cs
@@ -0,0 +1,19 @@
+﻿using Backend.Data;
+using Backend.Interfaces;
+using Backend.Models;
+
+namespace Backend.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context) {
+            _context = context;
+        }
+         
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+    }
+}

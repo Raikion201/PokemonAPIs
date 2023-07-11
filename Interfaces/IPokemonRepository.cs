3f28345b54a703b2573ca59db035602f1ad0d839
raikion201 <haitrieukt123@gmail.com>
2023-07-11 09:57:48 +0700

    rating-route

--- Interfaces/IPokemonRepository.cs
+++ Interfaces/IPokemonRepository.cs
@@ -4,6 +4,10 @@ namespace Backend.Interfaces
 {
     public interface IPokemonRepository
     {
-        ICollection<Pokemon> GetPokemons(); 
+        ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExist(int pokeId);
     }
 }
--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -7,13 +7,35 @@ namespace Backend.Repository
     public class PokemonRepository : IPokemonRepository
     {
         private readonly DataContext _context;
-        public PokemonRepository(DataContext context) {
+        public PokemonRepository(DataContext context)
+        {
             _context = context;
         }
-         
+
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+
+            if (review.Count() <= 0)
+            {
+                return 0;
+            }
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemons.Any(p => p.Id == pokeId);
         }
     }
 }

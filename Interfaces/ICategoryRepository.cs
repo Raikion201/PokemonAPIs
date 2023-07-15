8f0f5ff6da92ffc01426db1562885598694c0595
raikion201 <haitrieukt123@gmail.com>
2023-07-15 12:34:46 +0700

    add-interface-category

---
+++ Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Models;
+
+namespace Backend.Interfaces
+{
+    public interface ICategory
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExists(int id);
+
+    }
+}

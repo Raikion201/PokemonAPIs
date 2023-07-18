17706bd823e393c07e9602938994c8c7ac16775a
raikion201 <haitrieukt123@gmail.com>
2023-07-18 09:10:06 +0700

    review-repo

---
+++ Interfaces/IReviewerRepository.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Models;
+
+namespace Backend.Interfaces
+{
+    public interface IReviewerRepository
+    {
+        Reviewer GetReviewer(int reviewerId);
+        ICollection<Reviewer> GetReviewers();
+        ICollection<Review> GetReviewsByReviewer(int reviewerId);
+        bool ReviewerExists(int reviewerId);
+    }
+}
---
+++ Repository/ReviewerReposiotry.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Backend.Data;
+using Backend.Interfaces;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Repository
+{
+    public class ReviewerReposiotry : IReviewRepository
+    {
+        private readonly DataContext _context;
+        public ReviewerReposiotry(DataContext context, IMapper mapper)
+        {
+            _context = context;
+        }
+
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _context.Reviewers.Where(r => r.Id == reviewerId).Include(e => e.Reviews).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.ToList();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            return _context.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}

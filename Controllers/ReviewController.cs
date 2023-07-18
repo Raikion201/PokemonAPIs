2de40eb3da6ff42cea45a8efb47bc017fb09e6d3
raikion201 <haitrieukt123@gmail.com>
2023-07-18 09:03:18 +0700

    add-review-contrller

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -1,6 +1,58 @@
-﻿namespace Backend.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Backend.Interfaces;
+using Backend.Models;
+using Backend.Dto;
+
+namespace Backend.Controllers
 {
-    public class ReviewController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : Controller
     {
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewController(IReviewRepository reviewRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _reviewRepository = reviewRepository;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Review>))]
+        public IActionResult GetReviews()
+        {
+            var reviews = _mapper.Map<List<ReviewDto>>(_reviewRepository.GetReviews());
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(reviews);
+        }
+
+        [HttpGet("{reviewId}")]
+        [ProducesResponseType(200,Type = typeof(Review))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId))
+            {
+                return NotFound();
+            }
+            var review = _mapper.Map<Review>(_reviewRepository.GetReview(reviewId));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(review);
+        }
+
+        [HttpGet("/pokemon/{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Review>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewsOfAPokemon(int pokeId)
+        {
+            var reviews = _mapper.Map<List<ReviewDto>>(_reviewRepository.GetReviewsOfAPokemon(pokeId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(reviews);
+        }
     }
 }
